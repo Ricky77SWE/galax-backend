83d0a2f7e41478a188236668597aec132cf49713
Ricky77SWE <rickard.torn@hotmail.com>
2026-03-04 23:31:16 +0100

    Add import for request module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
+import request
 import requests
 import itertools
 import random
